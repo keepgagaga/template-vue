5b0400aea228f614da08c01d197454bf0fc095a4
saulzh <z@ruiruizhang.com>
2021-04-27 00:20:39 +0800

    update some canvas

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-     <h3>UploadImage</h3>
-     <input type="file" id="imageUpload" multiple @change="handleFiles">
-     <img :src="imageUrl">
+     <input type="file" id="imageUpload" multiple @change="handleFiles" v-if="showInput">
+     <canvas id="canvas"></canvas>
   </div>
 </template>
 
@@ -11,20 +10,56 @@ export default {
     name: 'UploadImage',
     data() {
         return {
-            image: null,
-            imageUrl: null,
+            showInput: true,
+            imageWidth: 0,
+            imageHeight: 0,
         }
     },
     methods: {
         handleFiles(event) {
-            console.log(event.target.files);
-            let files = event.target.files[0].name;
+            console.log(event.target.files[0]);
+            this.showInput = false;
+            let file = event.target.files[0];
             const fileReader = new FileReader();
-            fileReader.addEventListener('load', () => {
-                this.imageUrl = fileReader.result;
+            fileReader.addEventListener('load', (e) => {
+                
+                this.drwaImage(e.target.result);
             })
-            fileReader.readAsDataURL(files[0])
-            this.image = files[0];
+           fileReader.readAsDataURL(file);
+        },
+
+        drwaImage(imageUrl) {
+            var canvas = document.getElementById('canvas');
+            
+            var ctx = canvas.getContext('2d');
+            
+            var img = new Image();
+            img.onload = function() {
+               
+                var width = document.body.clientWidth - 400;
+                var height = document.body.clientHeight - 200;
+                var scale = img.width / img.height;
+                var screenScale = width / height;
+
+                var imageWidth = 300;
+                var imageHeight = 300 / scale;
+
+                //拖拽图片等比例放大缩小的原理就是不断设置图片的宽高
+                
+                canvas.width = width;
+                canvas.height = height;
+                ctx.drawImage(img, (width - imageWidth) / 2 , (height - imageHeight) / 2, imageWidth, imageHeight);
+                ctx.beginPath();
+                ctx.moveTo((width - imageWidth) / 2, (height - imageHeight) / 2);
+                ctx.lineTo(width / 2 + imageWidth / 2, (height - imageHeight) / 2);
+                ctx.lineTo(width / 2 + imageWidth / 2, height / 2 + imageHeight / 2);
+                ctx.lineTo((width / 2 - imageWidth / 2), (height + imageHeight) / 2);
+                ctx.strokeStyle = 'rgb(255, 0, 0)';
+                ctx.stroke();
+
+                
+            }
+            img.src = imageUrl;
         }
     }
 }
@@ -33,5 +68,18 @@ export default {
 </script>
 
 <style>
-
+.show-image {
+    width: 400px;
+    height: 200px;
+}
+body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+canvas {
+    border: 1px solid red;
+}
 </style>
